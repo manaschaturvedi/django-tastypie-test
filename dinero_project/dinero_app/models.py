235ae14f78cf18e47935c50f5206f7f66125aa77
Manas Chaturvedi <manas.oid@gmail.com>
2017-10-26 20:57:45 +0530

    defined Projects model

--- dinero_project/dinero_app/models.py
+++ dinero_project/dinero_app/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Projects(models.Model):
+	project_name = models.CharField(max_length=255)
+	project_owner = models.CharField(max_length=255)
+	client = models.CharField(max_length=255)
+	industry = models.CharField(max_length=255)
+	tech_stack = models.CharField(max_length=255)
+	start_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+	end_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+	def __unicode__(self):
+
+		return self.project_name + ' - ' + self.client
