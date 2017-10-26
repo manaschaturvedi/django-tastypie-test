427a2766c7d94ba54c2d64d25e722006b1afac49
Manas Chaturvedi <manas.oid@gmail.com>
2017-10-26 21:17:16 +0530

    added search filter by project name and filter options in admin

--- dinero_project/dinero_app/admin.py
+++ dinero_project/dinero_app/admin.py
@@ -5,6 +5,9 @@ from .models import Projects
 class ProjectsAdmin(admin.ModelAdmin):
 	list_display = ['__unicode__','project_name','project_owner','client','industry',
 						'tech_stack','start_date','end_date']
+	search_fields = ['project_name']
+	list_filter = ['project_name','project_name','project_owner','client','industry',
+						'tech_stack']
 	class Meta:
 		model = Projects
 
